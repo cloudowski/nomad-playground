6e3fe5aebb5835a2a5536d18a23f913300ece44c
Tomasz Cholewa <slashroot@slashroot.eu>
2016-04-14 15:47:02 +0200

    Consul works, but still nomad registers checks in loop

--- provision/consul.pp
+++ provision/consul.pp
@@ -1,12 +1,24 @@
 class { '::consul':
+  version     => '0.6.4',
   config_hash => {
     'bootstrap_expect' => 1,
     'client_addr'      => '0.0.0.0',
     'data_dir'         => '/opt/consul',
     'datacenter'       => 'east-aws',
-    'log_level'        => 'INFO',
-    'node_name'        => 'server',
+    'log_level'        => 'debug',
+    'node_name'        => $::hostname,
     'server'           => true,
     'ui_dir'           => '/opt/consul/ui',
+    'advertise_addr'   => $::ipaddress_eth1,
+    'enable_syslog'   => true,
+    'start_join'   => ['10.14.14.11', '10.14.14.12', '10.14.14.13'],
+    'retry_join'   => ['10.14.14.11', '10.14.14.12', '10.14.14.13'],
   }
 }
+
+$servers = { 
+	'nomad1' => { 'ip' => '10.14.14.11' },
+	'nomad2' => { 'ip' => '10.14.14.12' },
+	'nomad3' => { 'ip' => '10.14.14.13' },
+}
+create_resources(host, $servers)
--- provision/consul.sh
+++ provision/consul.sh
@@ -1,5 +1,38 @@
 #!/bin/bash
 
+export LC_ALL=C
+
 puppet module install KyleAnderson-consul
 
-puppet apply /vagrant/provision/consul.pp
+if [ $HOSTNAME = "nomad1" ];then
+	bootstrap="'bootstrap_expect' => 1,"
+fi
+
+cat << EOF > /tmp/consul.pp
+class { '::consul':
+  version     => '0.6.4',
+  config_hash => {
+    $bootstrap
+    'client_addr'      => '0.0.0.0',
+    'data_dir'         => '/opt/consul',
+    'datacenter'       => 'east-aws',
+    'log_level'        => 'debug',
+    'node_name'        => \$::hostname,
+    'server'           => true,
+    'ui_dir'           => '/opt/consul/ui',
+    'advertise_addr'   => \$::ipaddress_eth1,
+    'enable_syslog'   => true,
+    'start_join'   => ['10.14.14.11', '10.14.14.12', '10.14.14.13'],
+    'retry_join'   => ['10.14.14.11', '10.14.14.12', '10.14.14.13'],
+  }
+}
+
+\$servers = { 
+	'nomad1' => { 'ip' => '10.14.14.11' },
+	'nomad2' => { 'ip' => '10.14.14.12' },
+	'nomad3' => { 'ip' => '10.14.14.13' },
+}
+create_resources(host, \$servers)
+EOF
+
+puppet apply /tmp/consul.pp
